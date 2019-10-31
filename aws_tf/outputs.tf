bc5cef78f0a52eb7c7ae35d614d3a8a670e38692
JP <19959240+jordanpotti@users.noreply.github.com>
2019-10-30 20:31:56 -0600

    Update outputs.tf

--- aws_tf/outputs.tf
+++ aws_tf/outputs.tf
@@ -13,3 +13,11 @@ output "instance_private_ip_addresses" {
 output "Private_SSH_Key_Value" {
   value = "${tls_private_key.temp_key.private_key_pem}"
 }
+
+output "s3_bucket" {
+  value = "${random_id.s3.hex}"
+}
+
+output "Username" {
+  value = "ubuntu"
+}
