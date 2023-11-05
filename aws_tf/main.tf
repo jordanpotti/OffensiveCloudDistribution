0893f6d292d7015514ac4b621c6c12c21e185dc3
Guy Halfon <guyh@wix.com>
2023-11-05 11:46:28 +0200

    acl fix

--- aws_tf/main.tf
+++ aws_tf/main.tf
@@ -111,7 +111,6 @@ resource "aws_s3_object" "object" {
   source = var.scan_list
   depends_on = [
     aws_s3_bucket.scanning_storage,
-    aws_s3_bucket_acl.scanning_storage_acl,
     aws_s3_bucket_ownership_controls.scanning_storage_ownership
   ]
 }
